c34e3ad3f4b200791839137a1c4dbd703091a0d9
Hermann Witte <witteh@bzz.ch>
2022-05-30 21:41:16 +0200

    front side "rough layout"

---
+++ src/components/course.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Course(){
+
+    return (
+        <div className="courseFeed">
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+            <div className="courseBox"><p>Dies wird ein Beitrag</p></div>
+        </div>
+    )
+}
+
+export default Course;
